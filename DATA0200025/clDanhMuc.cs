c78ed3ed6323faea79a21abea282c1411f03baac
Tuan Ngoc <tuanngoc83@gmail.com>
2020-10-02 00:00:30 +0700

    tuannn

---
+++ DATA0200025/clDanhMuc.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using DomainModel;
+using DomainModel.Controls;
+using System.Runtime.InteropServices;
+
+namespace DATA0200025
+{
+   public class clDanhMuc
+    {
+
+        public static DataTable GetDataTable(string sTenBang)
+        {
+            DataTable vR;
+            string SQL = "SELECT * FROM DC_DanhMuc WHERE iID_MaLoaiDanhMuc IN (SELECT iID_MaLoaiDanhMuc FROM DC_LoaiDanhMuc WHERE sTenBang=@sTenBang) ORDER BY sTen";
+            SqlCommand cmd = new SqlCommand(SQL);
+            cmd.Parameters.AddWithValue("@sTenBang", sTenBang);
+            vR = Connection.GetDataTable(cmd, CThamSo.iKetNoi);
+            cmd.Dispose();
+            return vR;
+        }
+        public static SelectOptionList GetDDL(string sTenBang)
+        {
+            DataTable dt = GetDataTable(sTenBang);
+            SelectOptionList  DDL = new SelectOptionList(dt, "iID_MaDanhMuc", "sTen");
+            return DDL;
+        }
+    }
+
+ 
+}
---
+++ DATA0200025/clLoaiDanhMuc.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DATA0200025
+{
+    public class clLoaiDanhMuc
+    {
+        public static String LoaiHinhThucKiemTra { get { return "LoaiHinhThucKiemTra"; } }
+    }
+}
