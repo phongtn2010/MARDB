7fa3299622aa32de77fcce9f602c651eb5a27865
Tuan Ngoc <tuanngoc83@gmail.com>
2020-10-01 23:58:49 +0700

    tuann

---
+++ APP0200025/Controllers/DangKyController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace APP0200025.Controllers
+{
+    public class DangKyController : Controller
+    {
+        // GET: DangKy
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Edit()
+        {
+            return View();
+        }
+    }
+}
