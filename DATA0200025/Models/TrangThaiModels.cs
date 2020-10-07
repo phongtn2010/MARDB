6f07f9591a3097c2b784d32eb235192f9559e1db
Tuan Ngoc <tuanngoc83@gmail.com>
2020-10-07 22:49:53 +0700

    Thêm hàm get trạng thái tiếp theo. anh em cài thêm dapper về dùng nhé

---
+++ DATA0200025/Models/TrangThaiModels.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DATA0200025.Models
+{
+  public  class TrangThaiModels
+    {
+        public int iID_MaTrangThai { get; set; }
+        public string sTen { get; set; }
+    }
+}
--- DATA0200025/clCommon.cs
+++ DATA0200025/clCommon.cs
@@ -1,12 +1,67 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DomainModel;
+using Dapper;
+using DATA0200025.Models;
 
 namespace DATA0200025
 {
-    class clCommon
+    public class clCommon
     {
+        public static int GetTrangThaiIdTiepTheo(int iID_MaDoiTuong, int iID_MaHanhDong, string sMessageType, string sMessageFunction)
+        {
+            int vR = 0;
+            string SQL = @"SELECT iID_MaTrangThaiSau FROM CNN25_ChuyenTrangThai 
+                            WHERE iID_MaDoiTuong=@iID_MaDoiTuong AND iID_MaHanhDong=@iID_MaHanhDong 
+                            AND sMessageType=@sMessageType AND sMessageFunction=@sMessageFunction";
+            SqlCommand cmd = new SqlCommand(SQL);
+            cmd.Parameters.AddWithValue("@iID_MaDoiTuong", iID_MaDoiTuong);
+            cmd.Parameters.AddWithValue("@iID_MaHanhDong", iID_MaHanhDong);
+            cmd.Parameters.AddWithValue("@sMessageType", sMessageType);
+            cmd.Parameters.AddWithValue("@sMessageFunction", sMessageFunction);
+            vR= Convert.ToInt32(Connection.GetValue(cmd, 0, CThamSo.iKetNoi));
+            cmd.Dispose();
+            return vR;
+        }
+        public static int GetTrangThaiIdTiepTheo(int iID_MaDoiTuong, int iID_MaHanhDong,int iID_MaTrangThaiTruoc)
+        {
+            int vR = 0;
+            string SQL = @"SELECT iID_MaTrangThaiSau FROM CNN25_ChuyenTrangThai 
+                            WHERE iID_MaDoiTuong=@iID_MaDoiTuong AND iID_MaHanhDong=@iID_MaHanhDong 
+                            AND iID_MaTrangThaiTruoc=@iID_MaTrangThaiTruoc";
+            SqlCommand cmd = new SqlCommand(SQL);
+            cmd.Parameters.AddWithValue("@iID_MaDoiTuong", iID_MaDoiTuong);
+            cmd.Parameters.AddWithValue("@iID_MaHanhDong", iID_MaHanhDong);
+            cmd.Parameters.AddWithValue("@iID_MaTrangThaiTruoc", iID_MaTrangThaiTruoc);
+            vR = Convert.ToInt32(Connection.GetValue(cmd, 0, CThamSo.iKetNoi));
+            cmd.Dispose();
+            return vR;
+        }
+        public static TrangThaiModels GetTrangThaiModelsTiepTheo(int iID_MaDoiTuong, int iID_MaHanhDong, int iID_MaTrangThaiTruoc)
+        {
+            int iID_MaTrangThai = GetTrangThaiIdTiepTheo(iID_MaDoiTuong, iID_MaHanhDong, iID_MaTrangThaiTruoc);
+            using (SqlConnection connect = new SqlConnection(Connection.ConnectionString))
+            {
+                string SQL = @"SELECT *  FROM CNN25_TrangThai 
+                            WHERE iID_MaTrangThai=@iID_MaTrangThai";
+                    TrangThaiModels results = connect.Query<TrangThaiModels>(SQL,new {iID_MaTrangThai = iID_MaTrangThai}).FirstOrDefault();
+                return results;
+            }
+        }
+        public static TrangThaiModels GetTrangThaiModelsTiepTheo(int iID_MaDoiTuong, int iID_MaHanhDong, string sMessageType, string sMessageFunction)
+        {
+            int iID_MaTrangThai = GetTrangThaiIdTiepTheo(iID_MaDoiTuong, iID_MaHanhDong, sMessageType, sMessageFunction);
+            using (SqlConnection connect = new SqlConnection(Connection.ConnectionString))
+            {
+                string SQL = @"SELECT *  FROM CNN25_TrangThai 
+                            WHERE iID_MaTrangThai=@iID_MaTrangThai";
+                TrangThaiModels results = connect.Query<TrangThaiModels>(SQL, new { iID_MaTrangThai = iID_MaTrangThai }).FirstOrDefault();
+                return results;
+            }
+        }
     }
 }
