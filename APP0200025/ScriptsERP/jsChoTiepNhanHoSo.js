6c85c56382aa58c510cc110f7d10a7cf306a5359
Tuan Ngoc <tuanngoc83@gmail.com>
2020-10-12 20:42:55 +0700

    tuannn

---
+++ APP0200025/ScriptsERP/jsChoTiepNhanHoSo.js
@@ -0,0 +1,33 @@
+﻿$(function () {
+    $("body").on("click", "#btnBoSungYeuCau", function () {
+       
+       
+        var formData = new FormData($("#formYeuCau")[0]);
+            //var OrderService = $("#OrderService").val();
+            $.ajax({
+                url: '/ChoTiepNhanHoSo/YeuCauBoXungSubmit',
+                type: 'POST',
+                data: formData,
+                async: false,
+                cache: false,
+                contentType: false,
+                enctype: 'multipart/form-data',
+                processData: false,
+                success: function (response) {
+                  //  $('#' + OrderService).text("Link");
+                   // $('#' + OrderService).attr("href", response);
+                    $('#YeuCauBoSung').modal('toggle');
+                },
+                error: function (response) {
+                    alert("Bạn cần xử lý mẫu trước");
+                    $('#responsive').modal('toggle');
+                }
+
+            });
+            return false;
+      
+     
+
+
+    });
+});
