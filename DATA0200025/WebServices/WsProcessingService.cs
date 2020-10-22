b2aa2142c409d490586ad71736030fca6ba4669b
Phạm Văn Phong <phongpv2@vnpt.vn>
2020-10-22 14:00:07 +0700

    yui

--- DATA0200025/WebServices/WsProcessingService.cs
+++ DATA0200025/WebServices/WsProcessingService.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using DATA0200025.DTO;
+using DATA0200025.Models.Enums;
 using DATA0200025.WebServices.XmlType;
 using System;
 using System.Collections.Generic;
@@ -20,23 +21,27 @@ namespace DATA0200025.WebServices
 
         public HoSoVM ReceiveRegistrationProfile(Envelope envelope)
         {
+            //var result;
+            //var body = envelope.Body;
 
-            var body = envelope.Body;
+            //var hoso = body.Content.RegistrationProfile;
+            //hoso.fiActive = true;
+            //hoso.fiHSStatus = (int)HSStatus.CHO_TIEP_NHAN;
 
-            var hoso = body.Content.RegistrationProfile;
-            hoso.fiActive = true;
-            hoso.fiHSStatus = (int)HSStatus.CHO_TIEP_NHAN;
-            var hosoModel = new HoSo();
-            hosoModel = _mapper.Map(hoso, hosoModel);
-            hosoModel.IdDepartment = 1;
-            var result = _hoSoService.Add(hosoModel);
+            ////Them vao bang Ho So
+            ////var hosoModel = new HoSo();
+            ////hosoModel = _mapper.Map(hoso, hosoModel);
+            ////hosoModel.IdDepartment = 1;
+            ////result = _hoSoService.Add(hosoModel);
 
-            _lichSuHoSoService.Create(hoso.fiNSWFileCode, envelope.Header.From, "Gửi hồ sơ mới", "",
-                (int)HSStatus.CHO_TIEP_NHAN);
+            ////Them vao bang lich su
+            //_lichSuHoSoService.Create(hoso.fiNSWFileCode, envelope.Header.From, "Gửi hồ sơ mới", "", (int)HSStatus.CHO_TIEP_NHAN);
 
-            _lichSuHoSoService.Save();
-            _hoSoService.Save();
-            return _mapper.Map(result, hoso);
+            //_lichSuHoSoService.Save();
+            //_hoSoService.Save();
+
+            //return _mapper.Map(result, hoso);
+            return null;
         }
     }
 }
