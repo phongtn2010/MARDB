72a71480f1dadf10097e2e3bb79e134013747b7c
Tuan Ngoc <tuanngoc83@gmail.com>
2020-11-06 00:39:11 +0700

    t

--- APP0200025/Controllers/HoSoDaPheDuyetGDKController.cs
+++ APP0200025/Controllers/HoSoDaPheDuyetGDKController.cs
@@ -47,16 +47,7 @@ namespace APP0200025.Controllers
 
             return View(models);
         }
-        /// <summary>
-        /// view màn hình TiepNhanHoSo
-        /// </summary>
-        /// <returns></returns>
-        [AcceptVerbs(HttpVerbs.Get)]
-        public ActionResult TiepNhanHoSo()
-        {
-            ViewData["DuLieuMoi"] = "0";
-            return View();
-        }
+    
 
         /// <summary>
         /// update data màn hình TiepNhanHoSo
@@ -117,69 +108,7 @@ namespace APP0200025.Controllers
             return RedirectToAction("Index");
         }
        
-        /// <summary>
-        /// update data màn hình TuChoiHoSo
-        /// </summary>
-        /// <param name="ParentID"></param>
-        /// <param name="MaHoSo"></param>
-        /// <returns></returns>
-        [Authorize, ValidateInput(false), HttpPost]
-        public ActionResult TuChoiHoSoSubmit()
-        {
-            String ParentID = "TC";
-            NameValueCollection values = new NameValueCollection();
-            HttpFileCollectionBase files = Request.Files;
-            string sTenNguoiTiepNhan = CString.SafeString(Request.Form[ParentID + "_sNoiDung"]);
-            string iID_MaHoSo = CString.SafeString(Request.Form[ParentID + "_iID_MaHoSo"]);
-            if (string.IsNullOrEmpty(sTenNguoiTiepNhan))
-            {
-                values.Add("err_sNoiDung", "Bạn nhập thông tin yêu cầu cần bổ xung");
-            }
-            if (values.Count > 0)
-            {
-                for (int i = 0; i <= (values.Count - 1); i++)
-                {
-                    ModelState.AddModelError(ParentID + "_" + values.GetKey(i), values[i]);
-                }
-                base.ViewData["DuLieuMoi"] = "0";
-                ViewData["iID_MaHoSo"] = CString.SafeString(iID_MaHoSo);
-                return View();
-            }
-            string sFileTemp = "";
-            for (int i = 0; i < files.Count; i++)
-            {
-                HttpPostedFileBase postedFile = files[i];
-                if (postedFile != null && postedFile.ContentLength > 0)
-                {
-                    string guid = Guid.NewGuid().ToString();
-                    string sPath = "/Uploads/File";
-                    DateTime TG = DateTime.Now;
-                    string subPath = TG.ToString("yyyy/MM/dd");
-                    string subName = TG.ToString("HHmmssfff") + "_" + guid;
-                    string newPath = string.Format("{0}/{1}", sPath, subPath);
-                    CImage.CreateDirectory(Server.MapPath("~" + newPath));
-                    sFileTemp = string.Format(newPath + "/{0}_{1}", subName, postedFile.FileName);
-                    string filePath = Server.MapPath("~" + sFileTemp);
-                    postedFile.SaveAs(filePath);
-                }
-            }
-            HoSoModels hoSo = clHoSo.GetHoSoById(Convert.ToInt32(iID_MaHoSo));
-            int iTrangThaiTiepTheo = clTrangThai.GetTrangThaiIdTiepTheo((int)clDoiTuong.DoiTuong.BoPhanMotCua, (int)clHanhDong.HanhDong.TraChuyenVienXuLyLai, hoSo.iID_MaTrangThai, hoSo.iID_MaTrangThaiTruoc);
-            bang.MaNguoiDungSua = User.Identity.Name;
-            bang.IPSua = Request.UserHostAddress;
-            bang.TruyenGiaTri(ParentID, Request.Form);
-            bang.DuLieuMoi = false;
-            bang.GiaTriKhoa = iID_MaHoSo;
-            bang.CmdParams.Parameters.AddWithValue("@iID_MaTrangThai", iTrangThaiTiepTheo);
-            bang.CmdParams.Parameters.AddWithValue("@iID_MaTrangThaiTruoc", hoSo.iID_MaTrangThaiTruoc);
-            bang.Save();
-            clHoSo.CleanNguoiXem(iID_MaHoSo);
-            clLichSuHoSo.InsertLichSu(hoSo.iID_MaHoSo, User.Identity.Name, (int)clDoiTuong.DoiTuong.BoPhanMotCua, (int)clHanhDong.HanhDong.TuChoiHoSo, "Yêu cầu bổ sung hồ sơ", sFileTemp, hoSo.iID_MaTrangThai, iTrangThaiTiepTheo);
-            ResultModels result = new ResultModels { success = true };
-            result.value = Url.Action("Index");
-            return Json(result, JsonRequestBehavior.AllowGet);
-        }
-
+      
         public JsonResult Thoat(string iID_MaHoSo)
         {
             ResultModels result = clHoSo.CleanNguoiXem(iID_MaHoSo);
