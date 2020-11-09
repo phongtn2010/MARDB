e697991a3e95a15996aa0a795e9cf06d86405854
Phạm Văn Phong <phongpv2@vnpt.vn>
2020-11-09 14:42:11 +0700

    dsa

--- APP0200025/ScriptsERP/jsChoTiepNhanKetQua.js
+++ APP0200025/ScriptsERP/jsChoTiepNhanKetQua.js
@@ -1,9 +1,11 @@
 ﻿$(function () {
-    $("body").on("click", "#btnBoSungYeuCau", function () {    
-        var formData = new FormData($("#formYeuCau")[0]);
-            //var OrderService = $("#OrderService").val();
+    $("body").on("click", "#btnBoSungYeuCau", function () {
+        var bootstrapValidator = $("#formYeuCau").data('bootstrapValidator');
+        bootstrapValidator.validate();
+        if (bootstrapValidator.isValid()) {
+            var formData = new FormData($("#formYeuCau")[0]);
             $.ajax({
-                url: '/ChoTiepNhanKetQua/YeuCauBoSungSubmit',
+                url: ServerUrl + '/ChoXemXetKetQua/YeuCauBoSungSubmit',
                 type: 'POST',
                 data: formData,
                 async: false,
@@ -12,18 +14,24 @@
                 enctype: 'multipart/form-data',
                 processData: false,
                 success: function (response) {
-                    $('#responsive').modal('toggle');
+                    $('#YeuCauBoSung').modal('toggle');
                     if (response.success) {
+                        showToast_Success();
                         location.href = response.value;
+                    }
+                    else {
+                        showToast_Error();
                     }
                 },
                 error: function (response) {
-                    
-                    $('#responsive').modal('toggle');
+                    $('#YeuCauBoSung').modal('toggle');
+                    showToast_Error();
                 }
-
             });
             return false;
+        }
+        else return;
+
     });
 });
 
@@ -35,58 +43,21 @@ $(function () {
     });
 });
 
-
 $(function () {
-    $("body").on("click", ".opentuchoi", function () {
-
-        var iID_MaHangHoa = $(this).data("id");
-        var sMaHoSo = $(this).data("mahoso");
-        $("#TC_iID_MaHangHoa").val(iID_MaHangHoa);
-        $("#mahoso").html(sMaHoSo);
-    });
-});
-
-$(function () {
-    $("body").on("click", "#btnTuChoi", function () {
-        var formData = new FormData($("#formTuChoi")[0]);
-        //var OrderService = $("#OrderService").val();
+    $("body").on("click", "#btnThoat", function () {
+        var iID_MaHoSo = $("#Detail_iID_MaHangHoa").val();
         $.ajax({
-            url: '/ChoTiepNhanKetQua/TuChoiHoSoSubmit',
+            url: ServerUrl + '/ChoXemXetKetQua/Thoat',
             type: 'POST',
-            data: formData,
-            async: false,
-            cache: false,
-            contentType: false,
-            enctype: 'multipart/form-data',
-            processData: false,
+            data: { iID_MaHoSo: iID_MaHoSo },
             success: function (response) {
-                $('#TuChoi').modal('toggle');
                 if (response.success) {
+                    showToast_Success();
                     location.href = response.value;
                 }
             },
             error: function (response) {
-             
-                $('#responsive').modal('toggle');
-            }
-
-        });
-        return false;
-    });
-});
-
-$(function () {
-    $("body").on("click", "#btnThoat", function () {
-        var iID_MaHoSo = $("#Detail_iID_MaHoSo").val();
-        $.ajax({
-            url: '/ChoTiepNhanKetQua/Thoat',
-            type: 'POST',
-            data: { iID_MaHoSo: iID_MaHoSo},
-            success: function (response) {
-                
-                if (response.success) {
-                    location.href = response.value;
-                }
+                showToast_Error();
             }
         });
     });
--- APP0200025/ScriptsERP/jsChoXemXetKetQua.js
+++ APP0200025/ScriptsERP/jsChoXemXetKetQua.js
@@ -1,11 +1,9 @@
 ﻿$(function () {
     $("body").on("click", "#btnBoSungYeuCau", function () {    
-        var bootstrapValidator = $("#formYeuCau").data('bootstrapValidator');
-        bootstrapValidator.validate();
-        if (bootstrapValidator.isValid()) {
-            var formData = new FormData($("#formYeuCau")[0]);
+        var formData = new FormData($("#formYeuCau")[0]);
+            //var OrderService = $("#OrderService").val();
             $.ajax({
-                url: ServerUrl + '/ChoXemXetKetQua/YeuCauBoSungSubmit',
+                url: '/ChoXemXetKetQua/YeuCauBoSungSubmit',
                 type: 'POST',
                 data: formData,
                 async: false,
@@ -14,24 +12,18 @@
                 enctype: 'multipart/form-data',
                 processData: false,
                 success: function (response) {
-                    $('#YeuCauBoSung').modal('toggle');
+                    $('#responsive').modal('toggle');
                     if (response.success) {
-                        showToast_Success();
                         location.href = response.value;
-                    }
-                    else {
-                        showToast_Error();
                     }
                 },
                 error: function (response) {
-                    $('#YeuCauBoSung').modal('toggle');
-                    showToast_Error();
+                    
+                    $('#responsive').modal('toggle');
                 }
+
             });
             return false;
-        }
-        else return;
-        
     });
 });
 
@@ -43,21 +35,92 @@ $(function () {
     });
 });
 
+
 $(function () {
-    $("body").on("click", "#btnThoat", function () {
-        var iID_MaHoSo = $("#Detail_iID_MaHangHoa").val();
+    $("body").on("click", ".opentuchoi", function () {
+
+        var iID_MaHangHoa = $(this).data("id");
+        $("#TC_iID_MaHangHoa").val(iID_MaHangHoa);
+    });
+});
+
+$(function () {
+    $("body").on("click", "#btnTuChoi", function () {
+        var formData = new FormData($("#formTuChoi")[0]);
+        //var OrderService = $("#OrderService").val();
         $.ajax({
-            url: ServerUrl + '/ChoXemXetKetQua/Thoat',
+            url: '/ChoXemXetKetQua/TuChoiSubmit',
             type: 'POST',
-            data: { iID_MaHoSo: iID_MaHoSo},
+            data: formData,
+            async: false,
+            cache: false,
+            contentType: false,
+            enctype: 'multipart/form-data',
+            processData: false,
             success: function (response) {
+                $('#TuChoi').modal('toggle');
                 if (response.success) {
-                    showToast_Success();
                     location.href = response.value;
                 }
             },
             error: function (response) {
-                showToast_Error();
+             
+                $('#responsive').modal('toggle');
+            }
+
+        });
+        return false;
+    });
+});
+
+$(function () {
+    $("body").on("click", "#btnThoat", function () {
+        var iID_MaHoSo = $("#Detail_iID_MaHoSo").val();
+        $.ajax({
+            url: '/ChoXemXetKetQua/Thoat',
+            type: 'POST',
+            data: { iID_MaHoSo: iID_MaHoSo},
+            success: function (response) {
+                
+                if (response.success) {
+                    location.href = response.value;
+                }
+            }
+        });
+    });
+});
+
+
+$(function () {
+    $("body").on("click", "#btnDongY", function () {
+        var iID_MaHangHoa = $(this).data("id");
+        $.ajax({
+            url: '/ChoXemXetKetQua/DongY',
+            type: 'POST',
+            data: { iID_MaHangHoa: iID_MaHangHoa },
+            success: function (response) {
+
+                if (response.success) {
+                    location.href = response.value;
+                }
+            }
+        });
+    });
+});
+
+
+$(function () {
+    $("body").on("click", "#btnThuHoi", function () {
+        var iID_MaHangHoa = $(this).data("id");
+        $.ajax({
+            url: '/ChoXemXetKetQua/ThuHoi',
+            type: 'POST',
+            data: { iID_MaHangHoa: iID_MaHangHoa },
+            success: function (response) {
+
+                if (response.success) {
+                    location.href = response.value;
+                }
             }
         });
     });
